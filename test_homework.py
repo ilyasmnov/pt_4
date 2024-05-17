26458b4c02c0fabce342de2c300fcf09b36479d6
ilyasmnov <vk01b@sfolkar.com>
2024-05-17 17:25:43 +0300

    Исправления_2

--- test_homework.py
+++ test_homework.py
@@ -52,7 +52,7 @@ def test_random_list():
     Создайте список из 10 случайных чисел от 1 до 100 (включая обе границы) и отсортируйте его по возрастанию.
     """
 
-    l = sorted([random.randint(1, 101) for num in range(10)])
+    l = sorted([random.randint(1, 100) for num in range(10)])
 
     assert len(l) == 10
     assert all(l[i] <= l[i + 1] for i in range(len(l) - 1))
